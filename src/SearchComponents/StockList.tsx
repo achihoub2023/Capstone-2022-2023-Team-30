02283c9f5d8b0c73680a6cdad8419085e75236db
Mohamed Amine Smires <mohamedamines@outlook.com>
2022-03-10 14:36:40 -0500

    Overhaul of StockList
    Changed StockList to class component to support componentDidMount()

--- src/SearchComponents/StockList.tsx
+++ src/SearchComponents/StockList.tsx
@@ -1,44 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 // import './App.css';
 
 interface Props {
-    onStockClick: (stockName: string) => void
+  onStockClick: (stockName: string) => void
 }
 
-//Function that takes value in HTML element and calls onStockClick function
-function StockList({ onStockClick }: Props) {
-    function hello(e: any): void {
-        console.log(e.currentTarget.innerText)
-    }
+class StockList extends Component<Props> {
 
+  render(){
     return (
-        <div className="StockList">
-            {/* Placeholder for design */}
-            <ul id="list" onClick={(e) => hello(e)}>
-                <li
-                    onClick={(e) => {
-                        hello(e)
-                    }}
-                >
-                    Apple
-                </li>
-                <li
-                    onClick={(e) => {
-                        hello(e)
-                    }}
-                >
-                    Microsoft
-                </li>
-                <li
-                    onClick={(e) => {
-                        hello(e)
-                    }}
-                >
-                    Google
-                </li>
-            </ul>
-        </div>
+      <div className="StockList">
+
+        {/* Placeholder for design */}
+        <ul id="list"></ul>
+
+      </div>
     )
+  }
+
+  whenStockNameClicked(e: React.MouseEvent<HTMLLIElement, MouseEvent>): void {
+    console.log(e.currentTarget.innerText)
+    this.props.onStockClick(e.currentTarget.innerText)
+  }
+
+  // //Function to add elements to the list
+  addStocks(stockNames: string[]): void {
+    for (const stock in stockNames) {
+      const stockList = document.getElementById("list")!
+      const li = document.createElement("li")
+      li.innerText = stockNames[stock]
+      li.addEventListener("click", (e: any) => this.whenStockNameClicked(e))
+      console.log(li)
+      stockList.appendChild(li)
+    }
+  }
+
+  //Ran on component launch
+  componentDidMount(): void {
+    this.addStocks(["Apple", "Google", "Microsoft", "Amazon"])
+    console.log("StockList mounted")
+  }
+
 }
+
 
 export default StockList
