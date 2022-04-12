dff6a56ddcfebbbe1068ddd17843aa877ecb80b7
Anis Chihoub <anischihoub@nbp-213-120.nbp.ruw.rutgers.edu>
2022-04-12 02:14:15 -0400

    update flask app to meet front end requirements

--- backend/base.py
+++ backend/base.py
@@ -1,8 +1,27 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 
-api = Flask(__name__)
+app = Flask(__name__)
+app.debug = False
+CORS(app)
 
-@api.route('/profile')
+
+@app.route('/hello_route')
+def index():
+    return "Hello, World!"
+
+
+@app.route('/bye_route')
+def give_rating():
+    return "Bye, World!"
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("HI WEBSERVER")
+    app.run(host="0.0.0.0", port=5001)
+
 def my_profile():
     #basis for Get requests
     response_body = {
