79a1d1ace4bb0878e005683b30ae6d9be7c30557
Rahul Hegde <rahulhegde@nbp-133-211.nbp.ruw.rutgers.edu>
2022-04-20 16:41:43 -0400

    added search page to routes and fixed error thrown from header widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "pages/LandingPage";
 import Layout from "components/general/Layout";
 import "App.css";
 import Tutorial from "pages/Tutorial";
+import SearchPage from "pages/SearchPage";
+import { FStringSetter } from "interfaces";
 
 export default function App() {
+  const [stock, setStock] = useState("");
+
+  const setStockName: FStringSetter = (stockName: string) => {
+    setStock(stockName);
+  };
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/pages/*" element={<Layout />}>
           <Route path="tutorial" element={<Tutorial />} />
+          <Route
+            path="search"
+            element={<SearchPage setStockName={setStockName} />}
+          />
         </Route>
       </Routes>
     </Router>
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,5 +1,6 @@
 import React from "react";
+export type FStringSetter = (name: string) => void;
 export interface IHeaderContext {
   heading: string;
-  setHeading: React.Dispatch<React.SetStateAction<string>>;
+  setHeadingName: FStringSetter;
 }
